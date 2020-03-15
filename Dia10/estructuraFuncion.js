165fea3476240fb20542af8683246c5e3759dc2b
IPalos <ignaciopalos.r@gmail.com>
2020-03-15 01:06:21 -0600

    funciones ejercicios

---
+++ Dia10/estructuraFuncion.js
@@ -0,0 +1,18 @@
+function calcularAreaTriangulo(base, altura) {
+
+  var area = base * altura / 2
+  return area
+
+}
+
+// Esto devuelve la DEFINICIÓN de la función
+console.log( calcularAreaTriangulo )
+
+// Esto devuelve el RESULTADO de lo que hizo la función
+console.log ( calcularAreaTriangulo() )
+
+// Si tu función tiene parámetros, se invoca así
+console.log( calcularAreaTriangulo(3,5) )
+
+
+
---
+++ Dia10/funciones_RESUELTO.js
@@ -0,0 +1,11 @@
+function Convert_Fahrehneit_Celcius(value){
+  return (value-32)*(5/9)
+}
+
+function CalcarAreaCirculo(radius){
+  return 3.14*radius*radius
+}
+
+function ConvertTooURL(word){
+  return "www."+word+".com.mx"
+}
